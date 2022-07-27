4efac4862f2f1a87e4d0856ba4af453e0b5cb21d
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-27 22:47:19 +0200

    Add global middlerwares for security

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,22 +1,43 @@
 import cors from 'cors';
 import dotev from 'dotenv';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import globalErrorHandler from './controllers/errorController';
 import indexRouter from './routes';
+import AppError from './utils/AppError';
 
 dotev.config();
 
 const PORT = process.env.PORT!;
 const app = express();
 
-app.use(cors());
+// Global middleware
+app.use(cors()); // Access-Control-Allow-Origin
+app.use(helmet()); // Set security HTTP headers
+app.use(helmet.xssFilter()); // XSS-Protection
+
+app.use(express.json()); // Body parser, reading data from body into req.body
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  console.log('Start Development');
+  app.use(morgan('dev'));
+} else {
+  console.log('Start Production');
+}
 
 // Routes
+app.use('/api/v1', indexRouter);
 
-app.use('api/v1', indexRouter);
-
-app.get('/test', (req, res) => {
-  res.status(200).send('Hello World');
+// Global route
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
+// Global error handler middleware
+app.use(globalErrorHandler);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
