7b213aa182164efbf452fe28ce8158602ab707eb
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-28 03:39:28 +0200

    Read scoresList data from disk and send it

--- server/src/controllers/rankController.ts
+++ server/src/controllers/rankController.ts
@@ -1,11 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
+import { join } from 'path';
+import { ITestData } from '../interfaces/ITestData';
 import catchAsync from '../utils/catchAsync';
+import jsonFileReader from '../utils/readJsonFile';
 
 export const getRank = catchAsync(
-  (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { scoresList } = await jsonFileReader<ITestData>(
+      join(`${__dirname}../../../data/TestData.json`)
+    );
+
     res.status(200).json({
       status: 'success',
-      message: 'Hello from Rank',
+      scoresList,
     });
   }
 );
