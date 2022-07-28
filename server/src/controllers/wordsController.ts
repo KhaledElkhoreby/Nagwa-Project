63a317b26befc96db5d8341012ce684abe9e79f5
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-28 03:38:02 +0200

    Add Logic for generate random elements from word list

--- server/src/controllers/wordsController.ts
+++ server/src/controllers/wordsController.ts
@@ -1,11 +1,54 @@
 import { NextFunction, Request, Response } from 'express';
+import { join } from 'path';
+import { ITestData, IWordList, PartOfSpeech } from '../interfaces/ITestData';
 import catchAsync from '../utils/catchAsync';
+import jsonFileReader from '../utils/readJsonFile';
+
+// This function group words by their Part of Speech and return Map of key: ParOfSpeech => value: WordList
+const groupWordsArray = (wordList: IWordList[]) => {
+  const posArray = ['adverb', 'verb', 'noun', 'adjective'];
+  const groupsMap = new Map<PartOfSpeech, IWordList[]>();
+  posArray.forEach((pos) => {
+    groupsMap.set(
+      pos as PartOfSpeech,
+      wordList.filter((el) => el.pos === pos)
+    );
+  });
+  return groupsMap;
+};
+
+// This function return random element form an array
+const getRandomElementFromArray = <T>(arr: T[]) =>
+  arr[Math.floor(Math.random() * arr.length)];
+
+// This function take groups map and length of the returned array,
+// which returns an array of random elements from different groups which element from one group must appear at least one
+const randomElementsFromDifferentGroups = (
+  groupsMap: Map<PartOfSpeech, IWordList[]>,
+  length: number = 1
+) => {
+  const groupsArray = [...groupsMap];
+  let arr: IWordList[] = [];
+  for (let i = 0; i < length; i++) {
+    // Use (i % 4) to make sure the index not exceed the length of ['adverb', 'verb', 'noun', 'adjective']
+    arr.push(getRandomElementFromArray(groupsArray[i % 4][1]));
+  }
+  return arr;
+};
 
 export const getWords = catchAsync(
-  (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { wordList } = await jsonFileReader<ITestData>(
+      join(`${__dirname}../../../data/TestData.json`)
+    );
+
+    const groups = groupWordsArray(wordList);
+    const randomDifferentWords = randomElementsFromDifferentGroups(groups, 10);
+
     res.status(200).json({
       status: 'success',
-      message: 'Hello form words',
+      length: randomDifferentWords.length,
+      randomDifferentWords,
     });
   }
 );
