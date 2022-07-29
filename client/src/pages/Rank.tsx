9cc1f48dec8a8af0a8da67f2ab09afea5e9d93e3
Khaled El-Khoreby <elkhorebykhaled@gmail.com>
2022-07-29 17:59:17 +0200

    Fix bug for get rank of undifined score

--- client/src/pages/Rank.tsx
+++ client/src/pages/Rank.tsx
@@ -1,8 +1,13 @@
 import { useAppSelector } from '../app/hooks';
 import { useGetRankofScoreQuery } from '../services/rankApi';
-
+import { useNavigate } from 'react-router-dom';
 export default function Rank() {
+  const navigate = useNavigate();
   const score = useAppSelector(({ score }) => score.score);
+
+  // If the no score redirect to the practice page
+  if (!score) navigate('/');
+
   const { data, isError, isLoading } = useGetRankofScoreQuery(score);
   const rank = data?.rank;
   console.log({ score });
